0d4ecacfa04c6485dc4289d704ff995b52f7af3f
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-04-19 10:46:56 -0400

    Definition update

--- IDHIUtils.Core/Constants.cs
+++ IDHIUtils.Core/Constants.cs
@@ -1,9 +1,11 @@
 ﻿//
 // Constants
 //
+using System.Collections.Generic;
+
 namespace IDHIUtils
 {
-    static public class Constants
+    public static class Constants
     {
 #if KK
         public const string Prefix = "KK";
@@ -17,7 +19,7 @@ namespace IDHIUtils
         LieDown = 0,
         Stand = 1,
         SitChair = 2,
-        Stool = 3,
+        BacklessChair = 3,
         SofaBench = 4,
         BacklessBench = 5,
         SchoolDesk = 6,
@@ -30,5 +32,16 @@ namespace IDHIUtils
         MischievousCaress = 1003,
         Ground3P = 1100,
         AquariumCrowded = 1304,
+    }
+
+    public enum ErogenousZone
+    {
+        None = -1,
+        Lips = 0,
+        Chest = 1,
+        Crotch = 2,
+        Anus = 3,
+        Butt = 4,
+        Nipples = 5
     }
 }
