d11f27a5a618c57fe85fa8b4afb8e4127a6140b9
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-04-19 10:51:15 -0400

    Rename file

---
+++ IDHIUtils.Core/Utiliies.Scenes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+
+namespace IDHIUtils
+{
+    /// <summary>
+    /// Scene helpers
+    /// </summary>
+    public partial class Utilities
+    {
+        /// <summary>
+        /// Get name of active scene 
+        /// </summary>
+        /// <returns></returns>
+        public static string GetActiveScene()
+        {
+            return $"{Manager.Scene.ActiveScene.name}";
+        }
+
+        /// <summary>
+        /// Get overlay scene (dialogs, and stuff ...).
+        /// </summary>
+        public static string GetOverlaySceneName()
+        {
+#if KK
+            return Manager.Scene.Instance.AddSceneName;
+#else
+            return Manager.Scene.AddSceneName;
+#endif
+        }
+    }
+}
