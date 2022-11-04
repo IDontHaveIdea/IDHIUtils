c4e5518fc176876facd8efcd9ada37db268519a0
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-11-04 14:00:26 -0400

    Add AnimationLoader GetAnimationKey

---
+++ IDHIUtils.Core/AnimationLoader.cs
@@ -0,0 +1,30 @@
+﻿//
+// AnimationLoader
+//
+using System;
+
+
+namespace IDHIUtils
+{
+    /// <summary>
+    /// Class to get animation keys from AnimationLoader through reflection
+    /// </summary>
+    public class AnimationLoader : Utilities.PInfo
+    {
+        public AnimationLoader() : base("essuhauled.animationloader")
+        {
+        }
+
+        public string GetAnimationKey(HSceneProc.AnimationListInfo anim)
+        {
+            if (!Installed)
+            {
+                return "";
+            }
+
+            return Traverse.Method("GetAnimationKey",
+                new Type[] { typeof(HSceneProc.AnimationListInfo) })?
+                    .GetValue<string>(anim);
+        }
+    }
+}
