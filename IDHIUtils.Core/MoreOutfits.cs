4407efaa887648425ff9493bdb9b5038130878c1
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-11-04 14:01:00 -0400

    Add MoreOutfits get coordinate names

---
+++ IDHIUtils.Core/MoreOutfits.cs
@@ -0,0 +1,66 @@
+﻿//
+// MoreOutfits
+//
+using System.Collections.Generic;
+
+using HarmonyLib;
+
+
+namespace IDHIUtils
+{
+    /// <summary>
+    /// Class to get the coordinate names in MoreOutfits through reflection
+    /// </summary>
+    public class MoreOutfits : Utilities.PInfo
+    {
+        public MoreOutfits() : base("com.deathweasel.bepinex.moreoutfits")
+        {
+        }
+
+        internal object GetController(ChaControl chaControl)
+        {
+            if (!Installed)
+            {
+                return null;
+            }
+            return Traverse.Method("GetController",
+                    new object[] { chaControl }).GetValue();
+        }
+
+        public string GetCoordinateName(
+            ChaControl chaControl,
+            int coordinateIndex)
+        {
+            if (!Installed)
+            {
+                return "";
+            }
+
+            if (coordinateIndex < 3)
+            {
+                return $"{(ChaFileDefine.CoordinateType)coordinateIndex}";
+            }
+
+            return Traverse.Method("GetCoodinateName",
+                    new object[] { chaControl, coordinateIndex })
+                    .GetValue<string>();
+        }
+
+        public Dictionary<int, string> CoordinateNames(ChaControl chaControl)
+        {
+            return CoordinateNames(GetController(chaControl));
+        }
+
+        public Dictionary<int, string> CoordinateNames(object pluginController)
+        {
+            if (!Installed)
+            {
+                return new Dictionary<int, string>();
+            }
+
+            return Traverse.Create(pluginController)
+                .Field("CoordinateNames").GetValue<Dictionary<int, string>>()
+                    ?? new Dictionary<int, string>();
+        }
+    }
+}
