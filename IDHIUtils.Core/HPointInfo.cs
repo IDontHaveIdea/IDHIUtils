169e7ed1adb45af27f109c3a327ad127093a3758
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-11-30 11:04:48 -0500

    Add HPointInfo and MakerInfo

---
+++ IDHIUtils.Core/HPointInfo.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+using UnityEngine;
+using MessagePack;
+
+using H;
+
+using HarmonyLib;
+
+
+namespace IDHIUtils
+{
+#if KKS
+    public partial class HPointInfo
+    {
+        #region classes
+        public class Hooks
+        {
+            /// <summary>
+            /// Retrieve information for the HPointData on the map during HScene
+            /// </summary>
+            /// <param name="__instance">Instance of HSceneProc</param>
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(HSceneProc), nameof(HSceneProc.GetCloseCategory))]
+            private static void GetCloseCategoryPostfix(object __instance)
+            {
+                _hProcObject = __instance;
+                _hProcTraverse = Traverse.Create(__instance);
+                _closeHPointData = _hProcTraverse
+                    .Field<List<HPointData>>("closeHpointData").Value;
+                InitialPositon =
+                    _hProcTraverse.Field<Vector3>("nowHpointDataPos").Value;
+                InitialPositionName =
+                    _hProcTraverse.Field<string>("nowHpointData").Value;
+
+                _hPointDataPosition.Clear();
+                _hPointDataName.Clear();
+
+                foreach (var hpointData in _closeHPointData)
+                {
+                    _hPointDataPosition[hpointData.transform.position] = hpointData;
+                    _hPointDataName[hpointData.name] = hpointData;
+                }
+#if DEBUG
+                LogHPointDataInfo();
+#endif
+            }
+        }
+        #endregion
+
+        #region private Fields
+        private static Dictionary<Vector3, HPointData> _hPointDataPosition = new();
+        private static Dictionary<string, HPointData> _hPointDataName = new();
+        private static object _hProcObject = null;
+        private static Traverse _hProcTraverse = null;
+        private static List<HPointData> _closeHPointData;
+        #endregion
+
+        #region Properties
+        public static Dictionary<Vector3, HPointData> HPointByPosition
+        {
+            get
+            {
+                if (!HProcMonitor.Nakadashi)
+                {
+                    return null;
+                }
+                return _hPointDataPosition;
+            }
+        }
+        public static Dictionary<string, HPointData> HPointByName
+        {
+            get
+            {
+                if (!HProcMonitor.Nakadashi)
+                {
+                    return null;
+                }
+                return _hPointDataName;
+            }
+        }
+        public static Vector3 InitialPositon { get; private set; }
+        public static string InitialPositionName { get; private set; }
+        #endregion
+
+#if DEBUG
+        private static void LogHPointDataInfo()
+        {
+            var lines = new StringBuilder();
+            lines.Clear();
+
+            foreach (var hpointData in _closeHPointData)
+            {
+                lines.Append($"Name: {hpointData.name} " +
+                    $"position={hpointData.transform.position} " +
+                    $"rotation={hpointData.transform.rotation} " +
+                    $"angles={hpointData.transform.eulerAngles} " +
+                    $"backup position={hpointData.backupPos} ");
+                lines.Append($"categorys=" +
+                    $"{Utilities.CategoryList(hpointData.category, names: true)}\n");
+            }
+            Utilities._Log.Info($"\n\nHPointData:\n{lines}\n");
+        }
+#endif
+        public static void Init()
+        {
+            _ = Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+    }
+#endif
+}
---
+++ IDHIUtils.Core/MakerInfo.cs
@@ -0,0 +1,111 @@
+﻿//
+// MakerInfo
+//
+/*
+To comply with this rule, elements at the file root level or within a namespace should be
+positioned in the following order:
+
+Extern Alias Directives
+Using Directives
+Namespaces
+Delegates
+Enums
+Interfaces
+Structs
+Classes
+
+
+Within a class, struct or interface: (SA1201 and SA1203)
+
+Constant Fields
+Fields
+Constructors
+Finalizers (Destructors)
+Delegates
+Events
+Enums
+Interfaces (interface implementations)
+Properties
+Indexers
+Methods
+Structs
+Classes
+
+Within each of these groups order by access: (SA1202)
+
+public
+internal
+protected internal
+protected
+private
+
+Within each of the access groups, order by static, then non-static: (SA1204)
+
+static
+non-static
+
+Within each of the static/non-static groups of fields, order by readonly,
+then non-readonly : (SA1214 and SA1215)
+
+readonly
+non-readonly
+
+public static methods
+public methods
+internal static methods
+internal methods
+protected internal static methods
+protected internal methods
+protected static methods
+protected methods
+private static methods
+private methods
+
+The documentation notes that if the prescribed order isn't suitable - say, multiple
+interfaces are being implemented, and the interface methods and properties should be
+grouped together - then use a partial class to group the related methods and properties
+together.
+ */
+using UnityEngine.SceneManagement;
+
+
+namespace IDHIUtils
+{
+    public partial class MakerInfo
+    {
+        #region Properties
+        public static bool TitleRoot { get; private set; } = false;
+        public static bool ActionRoot { get; private set; } = false;
+        public static bool InMaker { get; private set; } = false;
+        public static bool InRoomMaker { get; private set; } = false;
+        #endregion
+
+        #region public Methods
+        public static void Init()
+        {
+            SceneManager.sceneLoaded += SceneLoaded;
+        }
+        #endregion
+
+        #region private Methods
+        private static void SceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == "Title")
+            {
+                TitleRoot = true;
+                ActionRoot = false;
+            }
+            if (scene.name == "Action")
+            {
+                ActionRoot = true;
+                TitleRoot = false;
+            }
+            if (scene.name == "CustomScene")
+            {
+                InMaker = TitleRoot;
+                InRoomMaker = ActionRoot;
+            }
+        }
+        #endregion
+    }
+}
