22f815053302a1bbcf8e1a0a3b71edfbc2bf5b91
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-01-26 18:07:24 -0500

    Extensions to get the map number where character is

---
+++ IDHIUtils.Core/Convenience/Extensions.cs
@@ -0,0 +1,34 @@
+﻿//
+// Extensions
+//
+
+using ActionGame.Chara;
+using KKAPI.MainGame;
+
+namespace IDHIUtils
+{
+    public static class UtilsExtensions
+    {
+        public static int MapNumber(this SaveData.Heroine self)
+        {
+            var nPC = self.GetNPC();
+            if (nPC != null)
+            {
+                return nPC.mapNo;
+            }
+
+            return -1;
+        }
+
+        public static int MapNumber(this ChaControl self)
+        {
+            var heroine = self.GetHeroine();
+            if (heroine != null)
+            {
+                return MapNumber(heroine);
+            }
+
+            return -1;
+        }
+    }
+}
