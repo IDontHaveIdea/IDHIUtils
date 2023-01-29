f4ac35f942bd387880b0a0fd0b0a2e793b55fb83
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-01-29 00:21:07 -0500

    Update plugin metadata references

--- IDHIUtils.KKS/Plugin.cs
+++ IDHIUtils.KKS/Plugin.cs
@@ -9,10 +9,7 @@ using KKAPI;
 namespace IDHIUtils
 {
     [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
-    [BepInPlugin(
-        IDHIUtils.Info.GUID,
-        IDHIUtils.Info.PluginName,
-        IDHIUtils.Info.Version)]
+    [BepInPlugin(GUID, PluginName, Version)]
     [BepInProcess(KoikatuAPI.GameProcessName)]
     [BepInProcess(KoikatuAPI.VRProcessName)]
     public partial class Utilities : BaseUnityPlugin
