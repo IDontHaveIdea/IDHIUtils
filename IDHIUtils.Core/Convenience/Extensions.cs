0783d11fe523a2c7754e3cfc210e59af78f454f0
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-02-12 02:49:30 -0500

    Added doc strings

--- IDHIUtils.Core/Convenience/Extensions.cs
+++ IDHIUtils.Core/Convenience/Extensions.cs
@@ -4,12 +4,21 @@
 using System;
 
 using UnityEngine;
+
 using KKAPI.MainGame;
+
 
 namespace IDHIUtils
 {
     public static class UtilsExtensions
     {
+        /// <summary>
+        /// String format Vector3 to help debugging 
+        /// </summary>
+        /// <param name="self">Reference to type Vector3</param>
+        /// <param name="decimals">Decimal places</param>
+        /// <param name="spaces">Total spaces used pad with space character</param>
+        /// <returns></returns>
         public static string Format(
             this Vector3 self,
             string decimals = default,
@@ -20,11 +29,19 @@ namespace IDHIUtils
             formatString = $"( " +
                 $"{string.Format($"{{0,{spaces}:{decimals}}}", self.x)}, " +
                 $"{string.Format($"{{0,{spaces}:{decimals}}}", self.y)}, " +
-                $"{string.Format($"{{0,{spaces}:{decimals}}}", self.z)} )";
+                $"{string.Format($"{{0,{spaces}:{decimals}}}", self.z)}" +
+                $" )";
 
             return formatString;
         }
 
+        /// <summary>
+        /// String format Vector2 to help debugging 
+        /// </summary>
+        /// <param name="self">Reference to type Vector3</param>
+        /// <param name="decimals">Decimal places</param>
+        /// <param name="spaces">Total spaces used pad with space character</param>
+        /// <returns></returns>
         public static string Format(
             this Vector2 self,
             string decimals = default,
@@ -34,11 +51,41 @@ namespace IDHIUtils
 
             formatString = $"( " +
                 $"{string.Format($"{{0,{spaces}:{decimals}}}", self.x)}, " +
-                $"{string.Format($"{{0,{spaces}:{decimals}}}", self.y)} )";
+                $"{string.Format($"{{0,{spaces}:{decimals}}}", self.y)}" +
+                $" )";
 
             return formatString;
         }
 
+        /// <summary>
+        /// String format Quaternion to help debugging
+        /// </summary>
+        /// <param name="self">Reference to type Quaternion</param>
+        /// <param name="decimals">Decimal places</param>
+        /// <param name="spaces">Total spaces used pad with space character</param>
+        /// <returns></returns>
+        public static string Format(
+                    this Quaternion self,
+                    string decimals = default,
+                    int spaces = default)
+        {
+            string formatString;
+
+            formatString = $"( " +
+                $"{string.Format($"{{0,{spaces}:{decimals}}}", self.x)}, " +
+                $"{string.Format($"{{0,{spaces}:{decimals}}}", self.y)}, " +
+                $"{string.Format($"{{0,{spaces}:{decimals}}}", self.z)}, " +
+                $"{string.Format($"{{0,{spaces}:{decimals}}}", self.w)}" +
+                $" )";
+
+            return formatString;
+        }
+
+        /// <summary>
+        /// Get map number where heroine is.
+        /// </summary>
+        /// <param name="self">Reference to Heroine object</param>
+        /// <returns></returns>
         public static int MapNumber(this SaveData.Heroine self)
         {
             var nPC = self.GetNPC();
@@ -50,6 +97,11 @@ namespace IDHIUtils
             return -1;
         }
 
+        /// <summary>
+        /// Get map number where heroine is
+        /// </summary>
+        /// <param name="self">Reference to ChaControl object</param>
+        /// <returns></returns>
         public static int MapNumber(this ChaControl self)
         {
             var heroine = self.GetHeroine();
