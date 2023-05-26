731d5ea2825739af13161233850c8da16aad55d4
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-05-26 00:52:45 -0400

    Use standard c# reflection

--- IDHIUtils.Core/Plugins/Pregnancy.cs
+++ IDHIUtils.Core/Plugins/Pregnancy.cs
@@ -1,8 +1,11 @@
 //
 // AnimationLoader
 //
+// Ignore Spelling: Utils
+
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 using UnityEngine;
 
@@ -11,20 +14,63 @@ namespace IDHIUtils
 {
     public class Pregnancy : Utilities.PInfo
     {
+        private readonly MethodInfo _fertilityMethod;
+        private readonly MethodInfo _statusMethod;
+
         public Pregnancy() : base("KK_Pregnancy")
         {
+            // GetFertility is a separate class PregnancyDataUtils in the KK_Pregnancy
+            // using regular reflection to access the method
+            var PregnancyType = Type.GetType("KK_Pregnancy.PregnancyDataUtils, KKS_Pregnancy");
+            if (PregnancyType != null)
+            {
+                _fertilityMethod = PregnancyType.GetMethod("GetFertility");
+                _statusMethod = PregnancyType.GetMethod("GetCharaStatus");
+            }
         }
 
-        public float? GetFertility(SaveData.CharaData character)
+        public float GetFertility(SaveData.CharaData character)
         {
+            var fertility = -1f;
+
             if (!Installed)
             {
-                return -1f;
+                return fertility;
             }
 
-            return Traverse.Method("GetFertility",
-                new Type[] { typeof(SaveData.CharaData) })?
-                    .GetValue<float>(character);
+            if (_fertilityMethod != null)
+            {
+                var obj = _fertilityMethod.Invoke(null, new object[] { character });
+                if (obj != null)
+                {
+                    fertility = (float)obj;
+                }
+            }
+
+            return fertility;
+            //return Traverse.Method("PregnancyDataUtils.GetFertility",
+            //    new Type[] { typeof(SaveData.CharaData) })?
+            //        .GetValue<float>(character);
+        }
+
+        public HeroineStatus GetCharaStatus(SaveData.CharaData character)
+        {
+            var status = HeroineStatus.Unknown;
+            if (!Installed)
+            {
+                return status;
+            }
+
+            if (_fertilityMethod != null)
+            {
+                var obj = _statusMethod.Invoke(null, new object[] { character, null });
+                if (obj != null)
+                {
+                    status = (HeroineStatus)(int)obj;
+                }
+            }
+
+            return status;
         }
     }
 }
