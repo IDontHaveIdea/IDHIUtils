25d495e3c1abd76f996b98dd5f51358b4bdbb1cd
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2024-08-17 19:29:30 -0400

    Embed LitJSON in library

---
+++ IDHIUtils.Core/LitJSON/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+
+/*
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle ("LitJson")]
+[assembly: AssemblyDescription ("LitJSON library")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("LitJSON")]
+[assembly: AssemblyCopyright (
+    "The authors disclaim copyright to this source code")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: AssemblyVersion("0.20.0.0")]
+
+[assembly: AssemblyFileVersion("0.20.0.0")]
+[assembly: AssemblyInformationalVersion("0.20.0-alpha.2+Branch.develop.Sha.aa1869387c0dbec44e56958ebbdc8ede8ea8c0e4")]
+*/
